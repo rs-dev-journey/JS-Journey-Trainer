3d583e4747d63b9c140f87903161b1d2d6d22b34
Sabina Batrakova <mms8818@mail.ru>
2026-03-18 21:07:27 +0100

    refactor: RSS-T-06 add changes in model.test.ts

--- js-journey-trainer/src/pages/auth/__tests__/model.test.ts
+++ js-journey-trainer/src/pages/auth/__tests__/model.test.ts
@@ -9,38 +9,38 @@ describe('validateInputForm', () => {
     );
     expect(errors.username).toBeTruthy();
   });
-});
 
-it('signup: invalid username format', () => {
-  const errors = validateInputForm(
-    { username: '!&', email: 'test.user@gmail.com', password: 'abc123' },
-    'signup',
-  );
-  expect(errors.username).toBeTruthy();
-});
+  it('signup: invalid username format', () => {
+    const errors = validateInputForm(
+      { username: '!&', email: 'test.user@gmail.com', password: 'abc123' },
+      'signup',
+    );
+    expect(errors.username).toBeTruthy();
+  });
 
-it('signin: username is not required', () => {
-  const errors = validateInputForm(
-    { username: '', email: 'test.user@gmail.com', password: 'abc123' },
-    'signin',
-  );
-  expect(errors.username).toBeUndefined();
-});
+  it('signin: username is not required', () => {
+    const errors = validateInputForm(
+      { username: '', email: 'test.user@gmail.com', password: 'abc123' },
+      'signin',
+    );
+    expect(errors.username).toBeUndefined();
+  });
 
-it('invalid email', () => {
-  const errors = validateInputForm(
-    { username: 'User1', email: 'uncorrect-email', password: 'abc123' },
-    'signup',
-  );
-  expect(errors.email).toBeTruthy();
-});
+  it('invalid email', () => {
+    const errors = validateInputForm(
+      { username: 'User1', email: 'uncorrect-email', password: 'abc123' },
+      'signup',
+    );
+    expect(errors.email).toBeTruthy();
+  });
 
-it('weak password', () => {
-  const errors = validateInputForm(
-    { username: 'User1', email: 'test.user@gmail.com', password: '123' },
-    'signup',
-  );
-  expect(errors.password).toBeTruthy();
+  it('weak password', () => {
+    const errors = validateInputForm(
+      { username: 'User1', email: 'test.user@gmail.com', password: '123' },
+      'signup',
+    );
+    expect(errors.password).toBeTruthy();
+  });
 });
 
 describe('getSupabaseErrorMessage', () => {
