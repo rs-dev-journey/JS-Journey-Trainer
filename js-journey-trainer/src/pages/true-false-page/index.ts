76df3e6f5d9854f12ebd504b08c2a2d96f5cc91c
Sabina Batrakova <mms8818@mail.ru>
2026-04-01 22:15:30 +0200

    feat: RSS-T-12 add loader and error state

--- js-journey-trainer/src/pages/true-false-page/index.ts
+++ js-journey-trainer/src/pages/true-false-page/index.ts
@@ -1,13 +1,43 @@
 import { getCurrentUserId } from '@/entities/user';
+import { renderErrorState } from '@/shared/ui/error-state';
+import { createLoader } from '@/shared/ui/loader';
 import { renderTrueFalseWidget, trueFalseQuestion } from '@/widgets/true-false';
 
-export function renderTrueFalsePage(root: HTMLElement): void {
-  const userId = getCurrentUserId();
+function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  if (!userId) {
-    throw new Error('User is not autenticated');
+export async function renderTrueFalsePage(root: HTMLElement): Promise<void> {
+  root.replaceChildren(createLoader());
+
+  try {
+    const temporaryDelay = 800;
+    await delay(temporaryDelay);
+
+    /* throw new Error('Test error');*/
+
+    const userId = getCurrentUserId();
+    if (!userId) {
+      throw new Error('User is not autenticated');
+    }
+
+    const questions = trueFalseQuestion;
+
+    if (questions.length === 0) {
+      throw new Error('Questions are empty');
+    }
+
+    const widget = renderTrueFalseWidget(userId, questions);
+    root.replaceChildren(widget);
+  } catch (error) {
+    root.replaceChildren(
+      renderErrorState({
+        title: 'Failed to open true-false',
+        message: error instanceof Error ? error.message : 'Unknown error',
+        onRetry: () => {
+          void renderTrueFalsePage(root);
+        },
+      }),
+    );
   }
-
-  const widget = renderTrueFalseWidget(userId, trueFalseQuestion);
-  root.append(widget);
 }
